559a7bd478af43401b6a737f511b35c732eb6a9d
Nickc123z <90527489+Nickc123z@users.noreply.github.com>
2021-09-12 20:25:11 -0400

    Add files via upload

---
+++ test/src/main/java/E1.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Nick Cook
+ */
+
+import java.util.Scanner;
+
+
+
+public class E1 {
+
+    public static void main(String[] args){
+
+
+        Scanner input = new Scanner (System.in);
+        System.out.println("What is your name?");
+        String First = input.next();
+        System.out.println("Hello " + First + ", nice to meet you friend");
+
+
+    }
+
+
+}
